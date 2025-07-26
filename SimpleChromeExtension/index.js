b89220b285347faea6d8335c75c292340743677b
OscarJGHB <Oscaar.Jiang@outlook.com>
2025-07-26 02:49:24 -0400

    added a delete button along with startApp function

--- SimpleChromeExtension/index.js
+++ SimpleChromeExtension/index.js
@@ -1,22 +1,33 @@
-const myLeads = []
+let myLeads = []
 
 //HTML ELEMENTS
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const outputEl = document.querySelector("#output-el")
+const deleteBtn = document.querySelector("#delete-btn")
+
+//fetch from localStorage
 
 inputBtn.addEventListener("click",function () {
     const inputStr = inputEl.value;
     if (!(inputStr.trim()  === "")) {
         myLeads.push(inputStr);
+        localStorage.setItem("leads", JSON.stringify(myLeads));
         inputEl.value = "";
-        render()
+        render(myLeads)
     }
 })
 
-function render(){
+deleteBtn.addEventListener("click",function () {
+    localStorage.removeItem("leads");
+    myLeads = []
+    render(myLeads)
+
+})
+
+function render(leads){
     let strOfLeads = ""
-    myLeads.forEach((lead) => {
+    leads.forEach((lead) => {
         strOfLeads +=
     `<li>
         <a href="${lead}" target = "_blank">
@@ -26,5 +37,15 @@ function render(){
     })
     outputEl.innerHTML = strOfLeads
 }
+
+function startApp(){
+    let leadsFromLocal = JSON.parse(localStorage.getItem("leads"))
+    if(leadsFromLocal && Array.isArray(leadsFromLocal)) {
+        myLeads = leadsFromLocal;
+    }
+    render(myLeads)
+}
+
+startApp()
 
 
